5e8adca3248a636368b032f7a41439ea4244dca7
Githuber765 <149252193+Githuber765@users.noreply.github.com>
2026-05-20 22:32:03 -0300

    Corregir hostname RabbitMQ

--- events/connection.py
+++ events/connection.py
@@ -2,6 +2,6 @@ import pika
 import os
 
 def get_rabbitmq_connection():
-    rabbitmq_url = os.environ.get("RABBITMQ_URL", "amqp://guest:guest@mysql:5672/")
+    rabbitmq_url = os.environ.get("RABBITMQ_URL", "amqp://guest:guest@rabbitmq:5672/")
     params = pika.URLParameters(rabbitmq_url)
     return pika.BlockingConnection(params)
